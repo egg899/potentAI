c038d389758dd3ada9cb013214241144cf9e1239
Alberto German Verissimo <germanverissimo5@hotmail.com>
2025-12-14 21:36:21 -0300

    cambio del vite.config.js otra vez

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: ['potentai.onrender.com']
   }
 })
